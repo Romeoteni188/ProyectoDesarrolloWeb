6654edeec0c1e0eeded1f123b6ac0686e5ff9f2c
Romeoteni188 <romeoteni093@gmail.com>
2023-09-29 23:04:28 -0600

    primera fase del proyecto

---
+++ bases/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+class Home(LoginRequiredMixin,generic.TemplateView):
+    template_name = 'bases/home.html' #'base/base.html'
+    login_url = '/admin'
